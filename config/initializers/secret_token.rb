0a387cd10ed942c6e03e2cd53b552c6452cb631f
Lloyd <lloyd@bunting.com.au>
2011-11-21 11:31:25 +1100

    Initial commit: currentreading.com

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Currentreading::Application.config.secret_token = 'd948afb0a2a4fb14695239c4a215a90ba6654c73ab2d7ec6278e7369dd769c1466d4a81ec23bec8593f928f25e1d5e64761e7423f30a9bbc165acbd98b69ab31'
